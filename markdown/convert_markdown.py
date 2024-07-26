98dbe8a937aad807583fe9fe0fdabc79c53c805d
snu1v3r <62672705+snu1v3r@users.noreply.github.com>
2024-07-26 13:16:24 +0200

    51 missing new line at end of readmemd (#53)
    * Check for newline at the end of the file
    
    * check also for empty line
    
    * fixed logic error
    
    * adds empty line to the file if necessary
    
    ---------
    
    Co-authored-by: snu1v3r <snu1v3r@github.com>

--- markdown/convert_markdown.py
+++ markdown/convert_markdown.py
@@ -114,7 +114,8 @@ def parse_markdown_files(tmp_file, md_files):
             with open(md_file, 'r') as f:
                 for line in f.readlines():
                     tmp_file.write(parse_line(line, md_file))
-                #tmp_file.write('\n') # This ensures that there is always an empty line between two markdown files ensuring proper chapters.
+                if line != '\n':
+                    tmp_file.write('\n')
 
 def execute_command(args):
     tmp_files = os.listdir('/tmp')
