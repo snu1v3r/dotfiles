b796eb0c90a2d9792e5c8aa868accc242580f6a1
user <user@localhost.localdomain>
2025-02-24 21:03:02 +0100

    small config changes

--- stowed_files/config/nvim/lua/custom/plugins/init.lua
+++ stowed_files/config/nvim/lua/custom/plugins/init.lua
@@ -1,4 +1,2 @@
-vim.opt.shada = "'50,<1000,s100,:0"
-
 return {
 }
--- stowed_files/config/nvim/lua/custom/plugins/options.lua
+++ stowed_files/config/nvim/lua/custom/plugins/options.lua
@@ -1,8 +1,11 @@
 -- Additional options for nvim
+vim.opt.shada = "'50,<1000,s100,:0"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.numberwidth = 4
 
 return {
+  vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "[B]uffer [N]ext" }),
+  vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "[B]uffer [P]revious" })
 
 }
