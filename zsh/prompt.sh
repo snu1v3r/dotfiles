1cffe549e94c4c8ede8882f40f2c0fbdb155a237
user <user@github.com>
2025-07-12 17:58:56 +0200

    fixed dotfiles dir

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,6 +1,6 @@
 # This file is used to include git status in the prompt
 # We start with this file, in order to be able to override stuff
-source ~/dotfiles/zsh/utils/git_prompt.sh
+source ${DOTFILES}/zsh/utils/git_prompt.sh
 
 # First we define several color code macro's
 
@@ -20,7 +20,7 @@ NEW_LINE=$'\n'
 
 # Select the various color's for the various items
 COLOR_LINES="$THEME_NORMAL%(#.$THEME_COLOR_BLUE.$THEME_COLOR_GREEN)" # Line color changes for root
-COLOR_USER="$THEME_BOLD%(#.$THEME_COLOR_RED.$THEME_COLOR_BLUE)" # User prompt color changer for root
+COLOR_USER="$THEME_BOLD%(#.$THEME_COLOR_RED.$THEME_COLOR_BLUE)"      # User prompt color changer for root
 COLOR_TIME="$THEME_NORMAL$THEME_COLOR_ORANGE"
 COLOR_VPN="$THEME_NORMAL$THEME_COLOR_ORANGE"
 COLOR_NORMAL="$THEME_COLOR_WHITE"
@@ -33,23 +33,22 @@ TIME="$ZSH_COLOR_TIME%D{%T}$COLOR_LINES"
 EXIT="%(?.$THEME_BOLD$THEME_COLOR_GREEN$NO_ERROR.$THEME_BOLD$THEME_COLOR_RED$ERROR:%?)$COLOR_LINES"
 GIT_PROMPT='$(git_super_status)'$COLOR_LINES
 FORMATTED_PATH="$THEME_BOLD$COLOR_NORMAL%(6~.%-1~/…/%4~.%5~)$COLOR_LINES"
-if [[ ! -z $(ip a show tun0 up 2>/dev/null) ]]
-then
-    TUN0=[$COLOR_VPN'$(ip addr show dev tun0 2>/dev/null | /usr/bin/grep -oP "inet [^/]+" | cut -d" " -f2)'$COLOR_LINES]
+if [[ ! -z $(ip a show tun0 up 2>/dev/null) ]]; then
+  TUN0=[$COLOR_VPN'$(ip addr show dev tun0 2>/dev/null | /usr/bin/grep -oP "inet [^/]+" | cut -d" " -f2)'$COLOR_LINES]
 else
-    TUN0=''
+  TUN0=''
 fi
 # Disable standard virtual env in prompt and put in custom
 
-venv_status () {
-    if [ ! -z $VIRTUAL_ENV ]
-    then 
-        echo -n '<'; basename -z $VIRTUAL_ENV; echo -n '>'
-    fi
+venv_status() {
+  if [ ! -z $VIRTUAL_ENV ]; then
+    echo -n '<'
+    basename -z $VIRTUAL_ENV
+    echo -n '>'
+  fi
 }
 export VIRTUAL_ENV_DISABLE_PROMPT=true
 VENV=$COLOR_NORMAL'$(venv_status)'$COLOR_LINES
 # Create the final prompt
 
 PROMPT="$COLOR_LINES┌─[$USER_AT_HOST]─[$TIME]─[$EXIT]─$TUN0$GIT_PROMPT$VENV─[$FORMATTED_PATH]$NEW_LINE$PROMPT_ICON$NORMAL "
-
