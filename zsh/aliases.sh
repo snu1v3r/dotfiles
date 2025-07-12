ad3614aa45a0b15c7e86f16b9c984f64de5a3888
user <user@github.com>
2025-07-12 21:29:47 +0200

    updated aliases
    updated aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -5,11 +5,11 @@ alias fzp="fzf --preview 'bat --style=numbers --colors=always --line-range :500 
 alias fzv="fzf --print0 | xargs -0 -o nvim"
 alias mc="mc --nosubshell"
 alias lg=lazygit
-alias la="eza -a"
-alias ll="eza --long --icons=always --git"
-alias ls="eza --icons=always"
-alias lt="eza --tree --icons=always"
-alias llt="eza --long --tree --icons=always --git"
+alias ls='eza -lh --group-directories-first --icons=auto'
+alias la='ls -a'
+alias lt='eza --tree --level=2 --long --icons --git'
+alias lta='lt -a'
+alias ltl='lt --long'
 alias vi='nvim'
 alias vim='nvim'
 alias svi='sudoedit'
@@ -18,11 +18,28 @@ alias man=batman
 alias diff=batdiff
 alias grep=rg
 alias grep='grep -nH --color=auto' # Changes the grep command to enable colors and print linenumbers and filenames for the hits
+alias ip='ip --color=auto'
 
 alias force_dvorak='setxkbmap dvorak' # Changes keyboard setting in X to dvorak. Needed to make VS code aware of layout
 alias force_us='setxkbmap us'         # Changes keyboard layout in X to US-International. Needed to make VS code aware of layout
 
 alias toggle_keyboard='test_keyboard=`setxkbmap -print | grep dvorak` ; if [[ $test_keyboard ]]; then setxkbmap us; else setxkbmap dvorak; fi' # This can be used to toggle the keyboard setting
+
+alias cd="zd"
+zd() {
+  if [ $# -eq 0 ]; then
+    builtin cd ~ && return
+  elif [ -d "$1" ]; then
+    builtin cd "$1"
+  else
+    z "$@" && printf " \U000F17A9 " && pwd || echo "Error: Directory not found"
+  fi
+}
+
+# Directories
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
 
 # This function enables a tail for a file with a specific search string. Time is printed when the string is found
 # Usage:    follow <file name to follow> <string to search for>
@@ -35,3 +52,6 @@ alias follow=myfunction
 alias clean_codes='sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"'
 
 alias convert-markdown='python3 ~/dotfiles/markdown/convert_markdown.py'
+
+# Find packages without leaving the terminal
+alias yayf="yay -Slq | fzf --multi --preview 'yay -Sii {1}' --preview-window=down:75% | xargs -ro yay -S"
--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -64,15 +64,6 @@ if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)" 
     export LS_COLORS="$LS_COLORS:ow=30;44:" # fix ls color for folders with 777 permissions 
  
-    alias ls='ls --color=auto' 
-    #alias dir='dir --color=auto' 
-    #alias vdir='vdir --color=auto' 
- 
-    alias grep='grep --color=auto' 
-    alias fgrep='fgrep --color=auto' 
-    alias egrep='egrep --color=auto' 
-    alias diff='diff --color=auto' 
-    alias ip='ip --color=auto' 
  
     export LESS_TERMCAP_mb=$'\E[1;31m'     # begin blink 
     export LESS_TERMCAP_md=$'\E[1;36m'     # begin bold 
