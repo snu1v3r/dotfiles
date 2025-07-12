1a734c5e2caf78b7bbbc779b0781eb85cbb62284
user <user@github.com>
2025-07-12 19:08:10 +0200

    various fixes for bat

--- themes/catppuccin/temp.sh
+++
@@ -1,4 +0,0 @@
-download_background_image() {
-  local url="$1"
-  curl -sL -o "background_01.png" "$url"
-}
--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,6 +1,5 @@
 alias saveclip='xclip -selection clipboard -t image/png -o > '
-alias cat=batcat
-alias bat=batcat
+alias cat=bat
 alias mount_shares='sudo vmhgfs-fuse .host:/ /mnt/hgfs -o allow_other'
 alias fzp="fzf --preview 'bat --style=numbers --colors=always --line-range :500 {}'"
 alias fzv="fzf --print0 | xargs -0 -o nvim"
@@ -21,15 +20,15 @@ alias grep=rg
 alias grep='grep -nH --color=auto' # Changes the grep command to enable colors and print linenumbers and filenames for the hits
 
 alias force_dvorak='setxkbmap dvorak' # Changes keyboard setting in X to dvorak. Needed to make VS code aware of layout
-alias force_us='setxkbmap us' # Changes keyboard layout in X to US-International. Needed to make VS code aware of layout
+alias force_us='setxkbmap us'         # Changes keyboard layout in X to US-International. Needed to make VS code aware of layout
 
 alias toggle_keyboard='test_keyboard=`setxkbmap -print | grep dvorak` ; if [[ $test_keyboard ]]; then setxkbmap us; else setxkbmap dvorak; fi' # This can be used to toggle the keyboard setting
 
 # This function enables a tail for a file with a specific search string. Time is printed when the string is found
 # Usage:    follow <file name to follow> <string to search for>
 myfunction() {
-	tail -f $1 | grep --colour --line-buffered $2 | awk '{print strftime("%c") " " $0}'
-	}
+  tail -f $1 | grep --colour --line-buffered $2 | awk '{print strftime("%c") " " $0}'
+}
 
 alias follow=myfunction
 
