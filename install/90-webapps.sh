c1bcf50ea1e98563183f53145dda875b44dbcba9
user <user@github.com>
2025-07-26 12:31:32 +0000

    fixed faulty directory after moving zsh config

--- install/90-webapps.sh
+++ install/90-webapps.sh
@@ -1,4 +1,4 @@
-source ~/.local/share/dotfiles/zsh/functions.sh
+source ~/.config/zsh/functions.sh
 web2app "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
 web2app "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
 web2app "Tijdloze" https://www.vrt.be/vrtmax/livestream/audio/tijdloze/
