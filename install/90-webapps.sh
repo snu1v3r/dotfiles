0781eba79ac437e5896916daf31eff2d939d9063
user <user@github.com>
2025-07-26 12:53:30 +0000

    fix .desktop files

--- install/90-webapps.sh
+++ install/90-webapps.sh
@@ -1,6 +1,9 @@
 source ~/.config/zsh/functions.sh
-web2app "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
-web2app "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
-web2app "Tijdloze" https://www.vrt.be/vrtmax/livestream/audio/tijdloze/
-web2app "Vuurland" https://www.vrt.be/vrtmax/livestream/audio/vuurland/
-web2app "NOS" https://nos.nl/
+# web2app can be used to creat desktop files for web applications this is not used because all .desktop files are available
+# through ~/.local/share/applications
+
+# web2app "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
+# web2app "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
+# web2app "Tijdloze" https://www.vrt.be/vrtmax/livestream/audio/tijdloze/
+# web2app "Vuurland" https://www.vrt.be/vrtmax/livestream/audio/vuurland/
+# web2app "NOS" https://nos.nl/
