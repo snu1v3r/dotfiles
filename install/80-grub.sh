8bac8f86b6f6a1b5803bc02cfb4437a3dcf47ca8
user <user@github.com>
2025-07-26 13:14:42 +0000

    support highres grub

--- install/80-grub.sh
+++ install/80-grub.sh
@@ -7,7 +7,11 @@ sudo cp -r ~/.local/share/themes/static/grub/* /boot/grub/themes
 sudo sed -i "s/^#\?\(GRUB_THEME=\).*$/\\1\"\/boot\/grub\/themes\/SekiroShadow\/theme.txt\"/" /etc/default/grub
 
 # This ensures that grub uses a higher graphix resolution
-sudo sed -i "s/^#\?\(GRUB_GFXMODE=\).*$/\\11920x1080/" /etc/default/grub
+if [[ "${RESOLUTION}" =~ "MULTI" ]]; then
+    sudo sed -i "s/^#\?\(GRUB_GFXMODE=\).*$/\\11920x1080/" /etc/default/grub
+else
+    sudo sed -i "s/^#\?\(GRUB_GFXMODE=\).*$/\\1${RESOLUTION}/" /etc/default/grub
+fi
 
 sudo sed -i "s/\(GRUB_CMDLINE_LINUX_DEFAULT=\"[^\"]*\)/\\1 splash/" /etc/default/grub
 # Update grub to enable updates
