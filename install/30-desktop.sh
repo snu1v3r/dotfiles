06d7041ff8ddfbfba9feae3a6de3daa2857ac819
user <user@github.com>
2025-07-27 15:58:12 +0000

    adds gnome keyring for nextcloud

--- install/30-desktop.sh
+++ install/30-desktop.sh
@@ -12,7 +12,7 @@ yay -S --noconfirm --needed \
 
 if [ "$PROFILE" = "main" ]; then
   yay -S --noconfirm --needed \
-    keepassxc nextcloud-client qt5-wayland brightnessctl \
+    keepassxc nextcloud-client qt5-wayland brightnessctl gnome-keyring \
     thunderbird virt-manager qemu-base qemu-desktop
 fi
 
