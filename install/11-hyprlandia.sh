5a550bcf122663a3664d51ee78e93c3c4bdf49b8
user <user@github.com>
2025-07-27 18:47:03 +0000

    load ssh agent in profile
    in previous setup ssh agent was loaded on first terminal login this
    loads the agent in the profile which makes the agent available through
    the entire login session

--- install/11-hyprlandia.sh
+++ install/11-hyprlandia.sh
@@ -1,9 +1,18 @@
 yay -S --noconfirm --needed \
   hyprland hyprshot hyprpicker hyprlock hypridle hyprpolkitagent hyprland-qtutils \
   wofi waybar mako swaybg \
-  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
 
-# Ensure that the zsh configuration is loaded from the .config/zsh directory
-# Start Hyprland on first session
-echo 'export ZDOTDIR=${HOME}/.config/zsh' >~/.zprofile
-echo "[[ -z \$DISPLAY && \$(tty) == /dev/tty1 ]] && exec Hyprland &>/dev/null" >>~/.zprofile
+tee ${HOME}/.zprofile &>/dev/null <<EOF
+# Loading the agent in the .zprofile makes it available during all sessions
+
+if [ -z "\$SSH_AUTH_SOCK" ] ; then
+    eval \$(ssh-agent -s) &>/dev/null
+fi
+
+# Putting zsh in the .config directory
+export ZDOTDIR=\${HOME}/.config/zsh
+
+# Loading Hyprland on boot
+[[ -z \$DISPLAY && \$(tty) == /dev/tty1 ]] && exec Hyprland &>/dev/null
+EOF
