138fb2771cbb526cd2a74f1ee2e4d067d781faa0
user <user@github.com>
2025-07-30 03:48:54 +0000

    installs network manager for wifi connection

--- install/30-desktop.sh
+++ install/30-desktop.sh
@@ -4,7 +4,7 @@ yay -S --noconfirm --needed \
   fcitx5 fcitx5-configtool fcitx5-gtk fcitx5-qt \
   evince imv \
   wl-clip-persist clipse sushi pamixer playerctl pavucontrol wireplumber \
-  pamac-git
+  pamac-git networkmanager network-manager-applet
 
 # yay -S --noconfirm --needed \
 #   spotify \
