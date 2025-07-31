dfa509db02c68e2f711ed476b6617b516f793513
user <user@github.com>
2025-07-31 19:07:16 +0000

    load bluetooth only on a host machine

--- install/21-bluetooth.sh
+++ install/21-bluetooth.sh
@@ -1,5 +1,10 @@
-# Install bluetooth controls
-yay -S --noconfirm --needed blueberry
 
-# Turn on bluetooth by default
-sudo systemctl enable --now bluetooth.service
+# Bluetooth is only needed on the main host. Other variants don't need this
+
+if [ "$PROFILE" = "main" ]; then
+    # Install bluetooth controls
+    yay -S --noconfirm --needed blueberry
+
+    # Turn on bluetooth by default
+    sudo systemctl enable --now bluetooth.service
+fi
