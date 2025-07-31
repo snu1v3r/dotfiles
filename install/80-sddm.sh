3d3e1e8e4dd0ed88ef7cfb02e7b421b64256389b
user <user@github.com>
2025-07-31 14:59:05 +0000

    fixup! login manager only for main/host vm

--- install/80-sddm.sh
+++ install/80-sddm.sh
@@ -6,8 +6,8 @@ if [ "${PROFILE}" = "main" ]; then
 
     sudo cp /usr/lib/sddm/sddm.conf.d/default.conf /etc/sddm.conf
     sudo sed -i "s/^\(Current=\).*/\\1mountain/" /etc/sddm.conf
+    sudo systemctl enable sddm.service
     sudo mkdir -p /etc/xdg/weston
-
     sudo tee /etc/xdg/weston/weston.ini &>/dev/null <<EOF
 [keyboard]
 keymap_layout=us
