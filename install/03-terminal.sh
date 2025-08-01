cc7aaecc13655a373147d8ec945e036e4fd08a10
user <user@github.com>
2025-08-01 09:31:10 +0000

    small fixes and updated for non debian standard

--- install/03-terminal.sh
+++ install/03-terminal.sh
@@ -1,29 +1,38 @@
 #!/usr/bin/env bash
 
-install_packages wget curl unzip fzf ripgrep zoxide bat btop man tldr less whois plocate zsh stow tmux openssh luarocks mc
+install_packages wget curl unzip fzf ripgrep zoxide bat btop man tldr less whois plocate zsh stow tmux luarocks mc
 
 case "${FLAVOR}" in
     "debian")
-        install_packages fd-find
+        install_packages fd-find openssh-client openssh-server p7zip
 
         # Install Yazi
         SUFFIX=x86_64-unknown-linux-musl
         TAGNAME=$(wget -qO- https://api.github.com/repos/sxyazi/yazi/releases/latest | jq -r .tag_name | cut -c2-)
-        wget -qO- https://github.com/sxyazi/yazi/releases/download/v${TAGNAME}/yazi-${SUFFIX}.zip | unzip -d /tmp
-        exit
+        wget -qO yazi.zip https://github.com/sxyazi/yazi/releases/download/v${TAGNAME}/yazi-${SUFFIX}.zip
+        unzip /tmp/yazi.zip -d /tmp
         sudo mv /tmp/yazi-${SUFFIX}/yazi /tmp/yazi-${SUFFIX}/ya /usr/bin
-        sudo apt install -y p7zip jq
-        exit
-        rm /tmp/yazi.zip
-        rm -rf /tmp/yazi-${SUFFIX}
-    log_success "Installed Yazi"
-        # eza
-        # bat-extras
-        # nvim
-        # yazi
+        rm -rf /tmp/yazi*
+
+        # Install neovim
+        wget -qO- https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz | tar xz -C /tmp
+        sudo cp -r /tmp/nvim-linux-x86_64/* /usr
+        rm -rf /tmp/nvim-linux-x86_64
+
+        # Install eza
+        wget -qO- https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz | tar xz -C /tmp
+        sudo mv -f /tmp/eza /usr/bin/eza
+      
+        # Intall bat-extras
+        BATURL=$(curl -s https://api.github.com/repos/eth-p/bat-extras/releases/latest | jq -r .assets\[0\].browser_download_url)
+        wget -qO /tmp/batextra.zip ${BATMANURL}
+        unzip /tmp/batextra.zip -d /tmp/bat
+        sudo mv /tmp/bat/* /usr
+        rm -rf /tmp/bat
+        rm /tmp/batextra.zip
         ;;
     "arch")
-        install_packages fd eza zoxide bat bat-extras \
+        install_packages fd eza zoxide bat bat-extras openssh \
           wl-clipboard fastfetch btop \
           nvim yazi swappy mc
         ;;
