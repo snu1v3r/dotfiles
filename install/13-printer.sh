7409ccd995397cbd45b29da9ab73bdf737891a18
user <user@github.com>
2025-08-01 18:31:49 +0000

    fixed printer
    boolean logic in code was wrong

--- install/13-printer.sh
+++ install/13-printer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Printer support is only needed on the main host
-if [ ! "${PROFILE}" = "main" ]; then
+if [ "${PROFILE}" = "main" ]; then
     install_packages cups cups-pdf cups-filters system-config-printer
     sudo systemctl enable cups.service
 fi
