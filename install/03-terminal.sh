439b5c768c91f6ee78b0e5dec2ab4208eb3239ed
user <user@github.com>
2025-08-01 08:59:17 +0000

    first step for terminal

--- install/03-terminal.sh
+++ install/03-terminal.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/env bash
-yay -S --noconfirm --needed \
-  wget curl unzip inetutils \
-  fd eza fzf ripgrep zoxide bat bat-extras \
-  wl-clipboard fastfetch btop \
-  man tldr less whois plocate \
-  alacritty zsh stow \
-  tmux openssh \
-  nvim luarocks tree-sitter-cli \
-  yazi swappy mc
+
+install_packages wget curl unzip fzf ripgrep zoxide bat btop man tldr less whois plocate zsh stow tmux openssh luarocks mc
+
+case "${FLAVOR}" in
+    "debian")
+        install_packages fd-find
+
+        # Install Yazi
+        SUFFIX=x86_64-unknown-linux-musl
+        TAGNAME=$(wget -qO- https://api.github.com/repos/sxyazi/yazi/releases/latest | jq -r .tag_name | cut -c2-)
+        wget -qO- https://github.com/sxyazi/yazi/releases/download/v${TAGNAME}/yazi-${SUFFIX}.zip | unzip -d /tmp
+        exit
+        sudo mv /tmp/yazi-${SUFFIX}/yazi /tmp/yazi-${SUFFIX}/ya /usr/bin
+        sudo apt install -y p7zip jq
+        exit
+        rm /tmp/yazi.zip
+        rm -rf /tmp/yazi-${SUFFIX}
+    log_success "Installed Yazi"
+        # eza
+        # bat-extras
+        # nvim
+        # yazi
+        ;;
+    "arch")
+        install_packages fd eza zoxide bat bat-extras \
+          wl-clipboard fastfetch btop \
+          nvim yazi swappy mc
+        ;;
+esac
 
 # Use stow to create links in the .local directory for dotfiles
 mkdir -p ${HOME}/.local/bin
