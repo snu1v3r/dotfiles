c6a88c8b2798b08f763022a3b2d1be442aaf56eb
user <user@github.com>
2025-08-23 19:59:16 +0000

    fixes printer
    fixes tar command for printer

--- install/24-printer.sh
+++ install/24-printer.sh
@@ -3,7 +3,7 @@
 if [ "${PROFILE}" = "main" ]; then
     install_packages cups cups-pdf cups-filters system-config-printer
     sudo systemctl enable cups.service
-    tar ${HOME}/.local/share/dotfiles/additional/Canon\ Printer\ Driver\ cque-en-4.0-12.x86_64.tar.gz /tmp
+    cd /tmp && tar xzvf ${HOME}/.local/share/dotfiles/assets/Canon\ Printer\ Driver\ cque-en-4.0-12.x86_64.tar.gz
     cd /tmp/cque-en-4.0-12 && sudo ./setup
     cd -
 fi
