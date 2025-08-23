c31c4c9636c933cee57a542bef319e4267dbc73d
user <user@github.com>
2025-08-23 16:28:12 +0000

    updated plymouth
    only load plymouth when in gui mode

--- install/52-plymouth.sh
+++ install/52-plymouth.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
-# This script is used to install and configure plymouth
 
-install_packages plymouth
+if [ ! "${PROFILE}" = "headless" ]; then
+    # This script is used to install and configure plymouth
 
-sudo mkdir -p /usr/share/plymouth/themes
-sudo cp -r ~/.local/share/themes/static/plymouth/* /usr/share/plymouth/themes
-sudo plymouth-set-default-theme arch-linux-branded -R
+    install_packages plymouth
 
-if [ -f /etc/mkinitcpio.conf ]; then
-    # This ensures that plymouth is hooked before encrypt
-    sudo sed -i "s/\(^HOOKS=([^)]*\)encrypt/\\1plymouth encrypt/" /etc/mkinitcpio.conf
+    sudo mkdir -p /usr/share/plymouth/themes
+    sudo cp -r ~/.local/share/themes/static/plymouth/* /usr/share/plymouth/themes
+    sudo plymouth-set-default-theme arch-linux-branded -R
 
-    # Regenerate boot image
-    sudo mkinitcpio -g /boot/initramfs-linux.img
+    if [ -f /etc/mkinitcpio.conf ]; then
+        # This ensures that plymouth is hooked before encrypt
+        sudo sed -i "s/\(^HOOKS=([^)]*\)encrypt/\\1plymouth encrypt/" /etc/mkinitcpio.conf
+
+        # Regenerate boot image
+        sudo mkinitcpio -g /boot/initramfs-linux.img
+    fi
+
 fi
