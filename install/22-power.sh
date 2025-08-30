60a42143630ed75e037c035e09ff2ef41e125d8a
user <user@github.com>
2025-08-30 18:22:45 +0000

    powerprofile only relevant for host machine

--- install/22-power.sh
+++ install/22-power.sh
@@ -1,17 +1,10 @@
 
 # Power control settings are not usefull in a headless environment, because this will be a container
 
-if [ ! "${PROFILE}" = "headless" ];then
+if [ "${PROFILE}" = "main" ];then
 # Setting the performance profile can make a big difference. By default, most systems seem to start in balanced mode,
 # even if they're not running off a battery. So let's make sure that's changed to performance.
     install_packages power-profiles-daemon
-
-    if ls /sys/class/power_supply/BAT* &>/dev/null; then
-      # This computer runs on a battery
-      powerprofilesctl set balanced
-    else
-      # This computer runs on power outlet
-      powerprofilesctl set performance
-    fi
+    powerprofilesctl set balanced
 fi
 
