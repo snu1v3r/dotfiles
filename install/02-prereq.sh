ef9354ff3e038bb383b0c4ae368172fb67290bf5
user <user@github.com>
2025-09-06 08:11:45 +0000

    moved pacman update

--- install/02-prereq.sh
+++ install/02-prereq.sh
@@ -1,13 +1,10 @@
-#!/usr/bin/env bash
-
-sudo sed -i 's/#\(\[multilib\]\)/\1\nInclude = \/etc\/pacman.d\/mirrorlist/g' /etc/pacman.conf
-sudo pacman -Syyu
-
 # stow is needed for activating the configuration directories
 install_packages stow
 
 # yay is used for the aur repo
 if [ ${DISTRO} = "arch" ]; then
+    sudo sed -i 's/#\(\[multilib\]\)/\1\nInclude = \/etc\/pacman.d\/mirrorlist/g' /etc/pacman.conf
+    sudo pacman -Syyu
     install_packages base-devel
     if ! command -v yay &>/dev/null; then
       git clone https://aur.archlinux.org/yay-bin.git
