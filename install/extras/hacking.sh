e1b67fb76421faac9e33b760a821f528ad54afdd
user <user@github.com>
2025-10-30 15:42:59 +0000

    updated system for extras

--- install/extras/hacking.sh
+++ install/extras/hacking.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-install_info "This is hacking"
+install-packages wireshark-qt ida-free ghidra ghidra-desktop gobuster netexec \
+	nmap nmap-netcat python-pwntools pwncat pwndbg checksec ropper wordlists \
+	dirbuster-wordlists hashcat hashcat-utils john metasploit 
