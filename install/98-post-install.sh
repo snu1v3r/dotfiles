2cc546c2f4c77c7899772742ce0b253c822190df
user <user@github.com>
2026-05-15 18:52:40 +0000

    fix service script
    fixes the moment where the service script runs (after login)

--- install/98-post-install.sh
+++ install/98-post-install.sh
@@ -17,29 +17,31 @@ sudo updatedb
 # This removes directory's I never use
 tee -a ${HOME}/first_boot.sh &>/dev/null <<EOF
 cd ~ && rm -rf Desktop Documents Music Pictures Public Templates Videos
-sudo systemctl disable first_boot.service
-#sudo rm /etc/systemd/system/first_boot.service
+systemctl --user disable first_boot.service
+echo "Post install" >> ${HOME}/install.log
+#rm "${HOME}/first_boot.service
 #rm "${HOME}/first_boot.sh"
 EOF
 
 
 # This creates the necessary service file
-sudo tee /etc/systemd/system/first_boot.service &>/dev/null <<EOF
+tee ${HOME}/first_boot.service &>/dev/null <<EOF
 [Unit]
 Description=First Boot Initialization Script
 ConditionPathExists=${HOME}/first_boot.sh
+After=default.target
 
 [Service]
 Type=oneshot
 ExecStart=${HOME}/first_boot.sh
 
 [Install]
-WantedBy=multi-user.target
+WantedBy=default.target
 EOF
 
 chmod +x "${HOME}/first_boot.sh"
 
 # This starts the necessary service for first boot
 
-sudo systemctl daemon-reload
-sudo systemctl enable first_boot.service
+systemctl --user daemon-reload
+systemctl --user enable first_boot.service
