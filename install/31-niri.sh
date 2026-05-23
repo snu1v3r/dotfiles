d793926d9078238de4d0873884ca5abe4acf7fc8
user <user@github.com>
2026-05-23 13:13:16 +0000

    fix overrides.kdl
    fix for screen resolution

--- install/31-niri.sh
+++ install/31-niri.sh
@@ -1,33 +1,9 @@
+#!/usr/bin/env bash
+
 if [ "${DISTRO}" = "arch" ] && [ "${DISPLAYMANAGER}" = "niri" ] && [ ! "${PROFILE}" = "headless" ]; then
-    tee ${HOME}/.config/niri/overrides.kdl &>/dev/null <<EOF
-# This file can be used to override monitor settings on a system specific level 
-# See https://wiki.hyprland.org/Configuring/Monitors/
+    tee "${HOME}/.config/niri/overrides.kdl" &>/dev/null <<EOF
+output "Virtual-1" {
 
-# Use single default monitor (see all monitors with: hyprctl monitors)
-# monitor= ,preferred,auto,auto
-# monitor = [name], preferred | <width>x<height>[@<frequency>],auto | <position right>x<position down>, auto | <scaling factor>
-
-# Example for Framework 13 w/ 6K XDR Apple display
-# monitor = DP-5, 6016x3384@60.00, auto, 2
-# monitor = eDP-1, 2880x1920@120.00, auto, 2
-#
-# Extra env variables
-# env = GDK_SCALE,1 # Change to 1 if on a 1x display
-#
-# Some suggestions
-#
-# Laptop only
-# monitor = , 2880x1800@60.00, auto, 1.6
-# env = GDK_SCALE, 1.6
-# 
-# Asus screen
-# monitor = , 1920x1080@60.00, auto, 1
-# env = GDK_SCALE, 1
-#
-# HP-monitors
-# monitor = , 2560x1440@60.00, auto, 1
-# env = GDK_SCALE, 1
-#
 EOF
     case "${RESOLUTION}" in
     "2880x1800")
@@ -37,12 +13,12 @@ EOF
       echo -e "# Resolution selected from install script\n\nmonitor = eDP-1, 2880x1800@120.00, auto, 1.6\nmonitor = DP-4, 2560x1440@60.00, auto, 1\nmonitor = DP-5, 2560x1440@60.00, auto, 1" >>~/.config/niri/overrides.kdl
       ;;
     *)
-      echo -e "# Resolution selected from install script\n\nmonitor = , ${RESOLUTION}@60.00, auto, 1\nenv = GDK_SCALE, 1" >>~/.config/niri/overrides.kdl
+      echo -e "// Resolution selected from install script\n\n   mode \"${RESOLUTION}@60.007\"\n   scale 1\n\n}" >>~/.config/niri/overrides.kdl
       ;;
     esac
     install_packages \
       niri xwayland-satellite xdg-desktop-portal-gnome xdg-desktop-portal-gtk noctalia-shell \
-	  slurp grim wl-clipboard
+	  slurp grim wl-clipboard satty
 fi
 
 
