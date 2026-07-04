c9360f4e3c0da7d4ad2d83bd36ff93260c98c282
user <user@github.com>
2026-07-04 11:45:04 +0000

    fixes config.toml

--- install/31-niri.sh
+++ install/31-niri.sh
@@ -22,10 +22,18 @@ EOF
 	  slurp grim wl-clipboard satty
 
 	install_packages dms-shell dms-shell-niri xdg-desktop-portal-gtk
-	# if [ "${PROFILE}" = "main" ]; then
-	# 	install_packages greetd-dms-greeter-git
-	# 	sudo systemctl enable greetd
-	# fi
+	if [ "${PROFILE}" = "main" ]; then
+		install_packages greetd-dms-greeter-git
+		sudo systemctl enable greetd
+		sudo tee "/etc/greetd/config.toml" &>/dev/null <<- EOF
+		[terminal]
+		vt = 1
+
+		[default_session]
+		user = "greeter"
+		command = "dms-greeter --command niri"
+		EOF
+	fi
 	systemctl --user add-wants niri.service dms
 fi
 
