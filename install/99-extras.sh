1f50d150e758199880273b98a0f175e4dfbd9d57
user <user@github.com>
2026-07-07 18:16:53 +0000

    install extras as extra function

--- install/99-extras.sh
+++ install/99-extras.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
-declare -a EXTRAS
 if gum confirm "Install extra's?" --default="no";  then
-	for FILE in ~/.local/share/dotfiles/install/extras/*; do
-		TMP="${FILE%.*}"
-		EXTRAS+=("${TMP##*/}")
-	done;
-	mapfile -t RESULT < <(gum choose "${EXTRAS[@]}" --no-limit --header="Choose the desired extras")
-	for EXTRA in "${RESULT[@]}"; do
-	    log_info "Starting ${EXTRA}.sh"
-		source "${HOME}/.local/share/dotfiles/install/extras/${EXTRA}.sh"
-	done;
+	install-extras
 fi
--- local/bin/helper-functions.sh
+++ local/bin/helper-functions.sh
@@ -216,3 +216,16 @@ saveclip() {
     fi
   fi
 }
+
+install-extras() {
+	declare -a EXTRAS
+	for FILE in ${HOME}/.local/share/dotfiles/install/extras/*; do
+		TMP="${FILE%.*}"
+		EXTRAS+=("${TMP##*/}")
+	done;
+	mapfile -t RESULT < <(gum choose "${EXTRAS[@]}" --no-limit --header="Choose the desired extras")
+	for EXTRA in "${RESULT[@]}"; do
+	    log_info "Starting ${EXTRA}.sh"
+		source "${HOME}/.local/share/dotfiles/install/extras/${EXTRA}.sh"
+	done;
+}
