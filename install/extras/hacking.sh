679e956fd750fbba62501c429903ee391bb6ab4d
snu1v3r <62672705+snu1v3r@users.noreply.github.com>
2026-07-18 16:04:59 +0000

    Update hacking.sh
    updates for ghidra

--- install/extras/hacking.sh
+++ install/extras/hacking.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-install_packages wireshark-qt ida-free ghidra ghidra-desktop gobuster netexec \
+install_packages wireshark-qt ida-free ghidra gobuster netexec \
 	nmap nmap-netcat python-pwntools pwncat pwndbg checksec ropper wordlists \
-	dirbuster-wordlists hashcat hashcat-utils john metasploit 
+	dirbuster-wordlists hashcat hashcat-utils john metasploit \
+	rz-cutter
+
+# Needed to ensure interface for ghidra is working
+echo "export _JAVA_AWT_WM_NONREPARENTING=1" >> "${HOME}/.config/zsh/environment_overrides.sh"
