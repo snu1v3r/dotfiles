161080e91e9aaac8168e5605d08c909905ff4e9c
snu1v3r <62672705+snu1v3r@users.noreply.github.com>
2026-07-18 10:46:07 +0000

    Update hacking.sh
    fix typo

--- install/extras/hacking.sh
+++ install/extras/hacking.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-install-packages wireshark-qt ida-free ghidra ghidra-desktop gobuster netexec \
+install_packages wireshark-qt ida-free ghidra ghidra-desktop gobuster netexec \
 	nmap nmap-netcat python-pwntools pwncat pwndbg checksec ropper wordlists \
 	dirbuster-wordlists hashcat hashcat-utils john metasploit 
