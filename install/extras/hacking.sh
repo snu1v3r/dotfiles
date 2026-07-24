36064f81043c8abbb74e81f91bdb1698196440c6
user <user@github.com>
2026-07-24 19:16:52 +0000

    add some fixes
    - small fixes for java based application
    - adds wxhexeditor

--- install/extras/hacking.sh
+++ install/extras/hacking.sh
@@ -2,7 +2,41 @@
 install_packages wireshark-qt ida-free ghidra gobuster netexec \
 	nmap nmap-netcat python-pwntools pwncat pwndbg checksec ropper wordlists \
 	dirbuster-wordlists hashcat hashcat-utils john metasploit \
-	rz-cutter
+	rz-cutter wxhexeditor
 
 # Needed to ensure interface for ghidra is working
 echo "export _JAVA_AWT_WM_NONREPARENTING=1" >> "${HOME}/.config/zsh/environment_overrides.zsh"
+# Needed to ensure scaling of the interface works
+echo "export _JAVA_OPTIONS=\"-Dsun.java2d.uiScale=2\"" >> "${HOME}/.config/zsh/environment_overrides.zsh"
+
+# Desktop file has environment variable to enable proper scaling
+# Desktop file has environment variable to fix empty interface
+sudo tee "/usr/share/applications/ghidra.desktop" &>/dev/null <<EOF
+[Desktop Entry]
+Type=Application
+Name=Ghidra
+Comment=A software reverse engineering framework
+Exec=env _JAVA_OPTIONS='-Dsun.java2d.uiScale=2',_JAVA_AWT_WM_NONREPARENTING=1 ghidra %F
+Icon=ghidra
+Categories=Development;Debugger;
+Terminal=False
+MimeType=application/x-ghidra-project;
+StartupNotify=true
+StartupWMClass=ghidra-Ghidra
+Keywords=ghidra,reverse,development,debugger,decompiler,disassembler,gdb,binary,analysis,engineering,security
+EOF
+
+
+# Desktop file has environment variable to enable proper scaling
+sudo tee "/usr/share/applications/burpsuite.desktop" &>/dev/null <<EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Burp Suite Free Edition
+Exec=env _JAVA_OPTIONS='-Dsun.java2d.uiScale=1.5' burpsuite
+Icon=burpsuite
+Comment=Burp Suite is an integrated platform for attacking web applications (free edition)
+Categories=Development;Security
+Terminal=false
+MimeType=application/x-extension-iml;
+EOF
